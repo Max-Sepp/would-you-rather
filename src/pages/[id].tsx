b40d5222d1d7ecd69c4f90f3eb4340545c862505
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2023-03-22 08:34:45 +0000

    updated

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,9 +1,10 @@
 import { type NextPage } from "next/types";
 
 const Question: NextPage = () => {
-    return (
-        <h1> question </h1>
-    )
+  return (
+    <h1> question </h1>
+  )
+  
 }
 
 export default Question
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,4 +36,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
