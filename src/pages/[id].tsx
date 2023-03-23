caf84ce5b72d9c62ee7fd1140111c14bd219d751
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2023-03-23 23:20:10 +0000

    added database question access

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,10 +1,51 @@
+import NextError from "next/error";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { type NextPage } from "next/types";
+import { useEffect, useState, type FunctionComponent, type MouseEvent } from "react";
+import { api } from "~/utils/api";
 
-const Question: NextPage = () => {
+type QuestionProps = {
+  leftOption: string,
+  rightOption: string,
+  leftPercentage: number,
+  rightPercentage: number,
+  currentQuestion: number
+}
 
-  
+const Question: FunctionComponent<QuestionProps> = ({leftOption, rightOption, leftPercentage, rightPercentage, currentQuestion}) => {
+
+  const [nextQuestion, setNextQuestion] = useState(1);
+  const [answered, setAnswered] = useState(false)
+
+  useEffect(() => {
+    const maxNumberQuestions = 5
+    let output: number = Math.floor(Math.random() * maxNumberQuestions) + 1
+
+    while (output == currentQuestion) {
+      output = Math.floor(Math.random() * maxNumberQuestions) + 1
+    }
+
+    setNextQuestion(output);
+    setAnswered(false)
+  }, [currentQuestion])
+
+  function submitLeftOption(e: MouseEvent) {
+    e.preventDefault();
+    if (!answered) {
+      setAnswered(true);
+      // TODO add trpc mutation code
+    }
+  }
+
+  function submitRightOption(e: MouseEvent) {
+    e.preventDefault();
+    if (!answered){
+      setAnswered(true);
+      // TODO add trpc mutation code
+    }
+  }
 
   return (
     <>
@@ -15,19 +56,19 @@ const Question: NextPage = () => {
       <div className="flex flex-col md:flex-row justify-center items-center gap-5">
         <div onClick={submitLeftOption} className="bg-slate-50 dark:bg-slate-700 m-2 p-2 rounded-xl text-slate-600 dark:text-slate-300 h-96 w-96 text-center flex flex-col items-center justify-center gap-4">
           <div className="text-xl">
-          {props.leftOption}
+          {leftOption}
           </div>
           <div className={`transition-all duration-300 ${answered ? "opacity-100" : "opacity-0"}`}>
-          {props.leftPercentage} %
+          {leftPercentage} %
           </div>
         </div>
         <div className="text-bold uppercase text-slate-900 dark:text-white my-auto text-3xl">OR</div>
         <div onClick={submitRightOption} className="bg-slate-50 dark:bg-slate-700 m-2 p-2 rounded-xl text-slate-600 dark:text-slate-300 h-96 w-96 text-center flex flex-col items-center justify-center gap-4">
           <div className="text-xl">
-          {props.rightOption}
+          {rightOption}
           </div>
           <div className={`transition-all duration-300 ${answered ? "opacity-100" : "opacity-0"}`}>
-          {props.rightPercentage} %
+          {rightPercentage} %
           </div>
         </div>
       </div>
@@ -41,4 +82,44 @@ const Question: NextPage = () => {
   
 }
 
-export default Question
+const QuestionPage: NextPage = () => {
+  const pageId = useRouter().query.id;
+
+  if (typeof pageId === "undefined") {
+    return (
+      <NextError 
+        title={"Could not find the question"}
+        statusCode={404}
+      />
+    )
+  }
+
+  const id: number = +pageId
+
+  const questionQuery = api.questions.getByID.useQuery({id})
+
+  if (questionQuery.error || typeof questionQuery.data === "undefined") {
+    return (
+      <NextError 
+        title={"Could not find the question"}
+        statusCode={404}
+      />
+    )
+  }
+  const data = questionQuery.data
+
+  const leftPercentage = Math.round((data.leftChosen / data.totalChosen) * 100 )
+  const rightPercentage =  100 - leftPercentage
+
+  return (
+    < Question
+      leftOption={data.leftQuestion} 
+      rightOption={data.rightQuestion} 
+      leftPercentage={leftPercentage}
+      rightPercentage={rightPercentage}
+      currentQuestion={id}
+    />
+  )
+}
+
+export default QuestionPage
