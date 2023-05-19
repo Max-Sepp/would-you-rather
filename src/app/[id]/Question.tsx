3b85f8050785d4fb4900e1b2c32995b0faa5d4ce
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2023-05-19 06:04:02 +0100

    added polling functionality

--- src/app/[id]/Question.tsx
+++ src/app/[id]/Question.tsx
@@ -3,8 +3,6 @@
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import Link from 'next/link';
 import { useEffect, useState, type MouseEvent } from "react";
-import NavBar from '~/components/NavBar';
-// import { api } from "@/utils/api";
 import { nextQuestion } from "~/utils/nextQuestion";
 
 type QuestionProps = {
@@ -16,9 +14,22 @@ type QuestionProps = {
   numQuestions: number
 }
 
+async function poll({ leftClicked, pageId }: { leftClicked: boolean, pageId: number }) {
+  await fetch("/api/questions/poll", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      leftChosen: leftClicked,
+      questionId: pageId
+    })
+  })
+}
+
 export default function Question({ leftOption, rightOption, leftPercentage, rightPercentage, pageId, numQuestions }: QuestionProps) {
 
-  // const questionMutation = api.questions.percentageUpdate.useMutation();
+
   const [answered, setAnswered] = useState(false);
 
   const [leftParent] = useAutoAnimate();
@@ -42,47 +53,40 @@ export default function Question({ leftOption, rightOption, leftPercentage, righ
     handleSessionStorage();
   }
 
-  // function submitLeftOption(e: MouseEvent) {
-  //   e.preventDefault();
-  //   if (!answered) {
-  //     setAnswered(true);
-  //     questionMutation.mutate({
-  //       pageId,
-  //       leftClicked: true
-  //     })
-  //   }
-  // }
+  function submitLeftOption(e: MouseEvent) {
+    e.preventDefault();
+    if (!answered) {
+      setAnswered(true);
+      poll({ leftClicked: true, pageId })
+    }
+  }
 
-  // function submitRightOption(e: MouseEvent) {
-  //   e.preventDefault();
-  //   if (!answered) {
-  //     setAnswered(true);
-  //     questionMutation.mutate({
-  //       pageId,
-  //       leftClicked: false
-  //     })
-  //   }
-  // }
+  function submitRightOption(e: MouseEvent) {
+    e.preventDefault();
+    if (!answered) {
+      setAnswered(true);
+      poll({ leftClicked: false, pageId })
+    }
+  }
 
   return (
     <>
       <h1 className="text-slate-900 dark:text-white text-center p-5 text-3xl md:text-5xl">Would You Rather?</h1>
       <div className="flex flex-col md:flex-row justify-center items-center gap-5">
-        <div ref={leftParent} className="bg-slate-50 dark:bg-slate-700 m-2 p-2 rounded-xl text-slate-600 dark:text-slate-300 h-44 md:h-96 w-72 lg:w-96 text-center flex flex-col items-center justify-center gap-4">
+        <div onClick={submitLeftOption} ref={leftParent} className="bg-slate-50 dark:bg-slate-700 m-2 p-2 rounded-xl text-slate-600 dark:text-slate-300 h-44 md:h-96 w-72 lg:w-96 text-center flex flex-col items-center justify-center gap-4">
           <div className="text-xl">
             {leftOption}
           </div>
           {answered ? <div>{leftPercentage} %</div> : null}
         </div>
         <div className="text-bold uppercase text-slate-900 dark:text-white my-auto text-xl md:text-3xl">OR</div>
-        <div ref={rightParent} className="bg-slate-50 dark:bg-slate-700 m-2 p-2 rounded-xl text-slate-600 dark:text-slate-300 h-44 md:h-96 w-72 lg:w-96 text-center flex flex-col items-center justify-center gap-4">
+        <div onClick={submitRightOption} ref={rightParent} className="bg-slate-50 dark:bg-slate-700 m-2 p-2 rounded-xl text-slate-600 dark:text-slate-300 h-44 md:h-96 w-72 lg:w-96 text-center flex flex-col items-center justify-center gap-4">
           <div className="text-xl">
             {rightOption}
           </div>
           {answered ? <div>{rightPercentage} %</div> : null}
         </div>
       </div>
-      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
       <Link href={`/${encodeURIComponent(nextPageId)}`}>
         <div onClick={nextPage} className="bg-slate-50 dark:bg-slate-700 my-2 mx-auto w-64 p-2 rounded-xl text-slate-600 dark:text-slate-300 text-center">
           Next Would You Rather?
