ddf889091a93a4d849f462e3d2fcabc7329c841f
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2023-05-22 17:19:25 +0100

    updated session storage already seen on homepage

--- src/app/TryWouldYouRather.tsx
+++ src/app/TryWouldYouRather.tsx
@@ -7,9 +7,19 @@ export default function TryWouldYouRather({ numQuestions }: { numQuestions: numb
 
   const nextPageId = nextQuestion(numQuestions);
 
+  const handleSessionStorage = () => {
+    let questionsVisited: number[] = []
+    const sessionQuestions = sessionStorage.getItem('ms-questions-visited');
+    if (sessionQuestions != null) {
+      questionsVisited = JSON.parse(sessionQuestions) as number[]
+    }
+    questionsVisited.push(nextPageId);
+    sessionStorage.setItem('ms-questions-visited', JSON.stringify(questionsVisited));
+  }
+
   return (
     <Link href={`/${encodeURIComponent(nextPageId)}`}>
-      <div className="bg-slate-50 dark:bg-slate-700 m-2 p-2 rounded-xl text-slate-600 dark:text-slate-300 h-44 md:h-96 w-72 lg:w-96 text-center flex items-center justify-center flex-none">
+      <div onClick={handleSessionStorage} className="bg-slate-50 dark:bg-slate-700 m-2 p-2 rounded-xl text-slate-600 dark:text-slate-300 h-44 md:h-96 w-72 lg:w-96 text-center flex items-center justify-center flex-none">
         Try a would you rather?
       </div>
     </Link>
